9fc0cfa08969ae3fa4a0f55bdf6318e699220750
joaoeymard <joaoeymard@gmail.com>
2024-08-24 01:08:22 -0300

    refatorando projeto

---
+++ dbMolduras.js
@@ -0,0 +1,34 @@
+const molduras = [
+  {
+    dominio: "brancarangel",
+    styleName: {
+      translateX: 1080 / 2,
+      translateY: 335,
+      rotate: (-6.81 * Math.PI) / 180,
+      background: "#0c67ce",
+      color: "white",
+    },
+    imagens: {
+      figureName: "./imagens/apoioaobrasil.png",
+      thumbnailName: "./imagens/apoioaobrasil.png",
+      figureFilter: "./imagens/mascaraBrasil.png",
+      thumbnailFilter: "./imagens/metadataBrasil.png",
+    },
+  },
+  {
+    dominio: "44000",
+    styleName: {
+      translateX: 1080 / 2,
+      translateY: 500,
+      rotate: 0,
+      background: "#0c67ce",
+      color: "white",
+    },
+    imagens: {
+      figureName: "./imagens/moldura.jpg",
+      thumbnailName: "./imagens/moldura.jpg",
+      // figureFilter: "./imagens/mascaraBrasil.png",
+      // thumbnailFilter: "./imagens/metadataBrasil.png",
+    },
+  },
+];
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const $main = document.querySelector("#main");
 const $menu = document.querySelector("#menu");
+const $inputUser = $main.querySelector("input[name=user]");
+
+// Variável para armazenar a moldura selecionada
+let moldura = null;
 
 // Carregamento da fonte personalizada
 const pantonFont = new FontFace(
@@ -15,29 +19,66 @@ pantonFont.load().then(function (font) {
 
 // Função para alternar entre as páginas
 function tooglePages() {
+  clearInputUser();
+
   $menu.classList.toggle("hidden");
   $main.classList.toggle("hidden");
 }
 
-// Carregamento de imagens
-const baseImageFigure = new Image();
-baseImageFigure.src = "./apoioaobrasil.png?v=2";
+function clearInputUser() {
+  $inputUser.type = "text";
+  $inputUser.value = "";
+  $inputUser.file = "";
+}
 
-const baseImageFiltro = new Image();
-baseImageFiltro.src = "./bannerInicialBrasil.png?v=2";
+// Crie um objeto URL a partir da string da URL
+const urlObj = new URL(window.location.href);
+
+// Use URLSearchParams para acessar os parâmetros da query string
+const params = new URLSearchParams(urlObj.search);
+
+// Verifique se o parâmetro "m" existe
+if (params.has("m")) {
+  moldura = molduras.find((m) => m.dominio === params.get("m"));
+}
+
+// Carregamento de imagens
+const baseImgEscolhaImagem = new Image();
+baseImgEscolhaImagem.src = "./assets/imgEscolhaImagem.png";
+
+const baseImageFigure = new Image();
+baseImageFigure.src = moldura.imagens.figureName;
 
 const baseImageMascara = new Image();
-baseImageMascara.src = "./mascaraBrasil.png?v=2";
+baseImageMascara.src = moldura.imagens.figureFilter;
 
 let baseImageUsuario = new Image();
 
+// Evento de carregamento da imagem de apoio ao Brasil
+if (moldura.imagens.thumbnailName) {
+  $menu.querySelector(".btnFigure img").src = moldura.imagens.thumbnailName;
+} else {
+  $menu.querySelector(".btnFigure").classList.add("hidden");
+}
+
+if (moldura.imagens.thumbnailFilter) {
+  $menu.querySelector(".btnFilter img").src = moldura.imagens.thumbnailFilter;
+} else {
+  $menu.querySelector(".btnFilter").classList.add("hidden");
+}
+
 // Eventos de clique nos botões
 document.querySelectorAll(".btnActions").forEach((b) => {
-  b.addEventListener("click", clickMenu, true);
+  b.addEventListener("click", clickMenu);
 });
-document.querySelector("input[type=text]").addEventListener("keyup", (ev) => {
+$inputUser.addEventListener("keyup", (ev) => {
   ev.preventDefault();
   drawFigureName();
+});
+$inputUser.addEventListener("change", (ev) => {
+  ev.preventDefault();
+
+  if ($inputUser.type === "file") drawFilterPhoto();
 });
 
 // Função que gera as ações dos cliques nos botões
@@ -48,53 +89,108 @@ function clickMenu(ev) {
   switch (mtype) {
     case "figure-name":
       tooglePages();
-      document.querySelector("input[name=yourname]").type = "text";
-      document.querySelector("input[name=yourname]").value = "";
+
+      $inputUser.type = "text";
       drawFigureName();
 
       break;
     case "filter-photo":
       tooglePages();
-      document.querySelector("input[name=yourname]").type = "file";
-      document.querySelector("input[name=yourname]").value = "";
-      document.querySelector("input[name=yourname]").file = "";
-      // drawArte();
+
+      $inputUser.type = "file";
+      drawFilterPhoto();
 
       break;
     case "page-back":
       tooglePages();
+
       break;
     case "save":
       saveImage(ev);
+
       break;
     case "share":
       shareImage(ev);
+
       break;
   }
 }
 
 // Função para desenhar a figura e o nome caso exista
 function drawFigureName() {
-  const $input = document.querySelector("input[name=yourname]");
+  const $input = $inputUser;
 
   context.drawImage(baseImageFigure, 0, 0);
   context.save();
 
   if ($input.value.trim()) {
-    context.translate(1080 / 2, 335);
-    context.rotate((-6.81 * Math.PI) / 180);
+    context.translate(
+      moldura.styleName.translateX,
+      moldura.styleName.translateY
+    );
+    context.rotate(moldura.styleName.rotate);
 
     context.textBaseline = "middle";
     context.font = "125px myPantonFont";
 
     const width = context.measureText($input.value.trim()).width;
 
-    context.fillStyle = "#0c67ce";
+    context.fillStyle = moldura.styleName.background;
     roundRect(context, -width / 2 - 45, -175 / 2, width + 45 * 2, 175, 21.66);
 
-    context.fillStyle = "white";
+    context.fillStyle = moldura.styleName.color;
     context.fillText($input.value.trim(), -width / 2, 0);
     context.restore();
+  }
+}
+
+// Função para desenhar a figura e a foto do usuário
+function drawFilterPhoto() {
+  const $input = $inputUser;
+
+  context.drawImage(baseImgEscolhaImagem, 0, 0);
+  context.drawImage(baseImageMascara, 0, 0);
+  context.save();
+
+  if ($input.files.length) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      context.clearRect(0, 0, 1080, 1080);
+
+      baseImageUsuario.src = e.target.result;
+      baseImageUsuario.onload = function () {
+        if (baseImageUsuario.width < baseImageUsuario.height) {
+          context.drawImage(
+            baseImageUsuario,
+            0,
+            (baseImageUsuario.height - baseImageUsuario.width) / 2,
+            baseImageUsuario.width,
+            baseImageUsuario.width,
+            0,
+            0,
+            1080,
+            1080
+          );
+        } else {
+          context.drawImage(
+            baseImageUsuario,
+            (baseImageUsuario.width - baseImageUsuario.height) / 2,
+            0,
+            baseImageUsuario.height,
+            baseImageUsuario.height,
+            0,
+            0,
+            1080,
+            1080
+          );
+        }
+
+        context.drawImage(baseImageMascara, 0, 0, 1080, 1080);
+      };
+    };
+
+    reader.readAsDataURL($input.files[0]);
   }
 }
 
@@ -130,30 +226,58 @@ async function shareImage() {
   try {
     gtag("event", "share");
 
+    const arqName = "figure" + new Date().getTime();
+
     canvas.toBlob((blob) => {
-      const filesArray = [
-        new File(
-          [blob],
-          document.querySelector("input").value.trim() + ".jpg",
-          {
+      const shareData = {
+        files: [
+          new File([blob], arqName + ".jpg", {
             type: "image/jpeg",
             lastModified: new Date().getTime(),
-          }
-        ),
-      ];
-
-      const shareData = {
-        files: filesArray,
+          }),
+        ],
       };
 
-      navigator.share(shareData);
-    }, "image/jpeg");
+      if (navigator.canShare && navigator.canShare(shareData)) {
+        navigator
+          .share(shareData)
+          .then(() => {
+            console.log("Compartilhado com sucesso!");
+          })
+          .catch((error) => {
+            alert("Erro ao compartilhar: ", error.message);
+          });
+      } else {
+        alert("Não é possível compartilhar.");
+      }
+    });
   } catch (error) {
-    console.error("Erro ao compartilhar a imagem:", error);
+    gtag("event", "exception", {
+      description: "[fn:shareImage] " + (error.message || error),
+      fatal: false,
+    });
+    alert(
+      "Não foi possível compartilhar a imagem: " + (error.message || error)
+    );
   }
 }
 
 // Função para salvar a imagem (a ser implementada)
 function saveImage() {
-  // Implementar a funcionalidade de salvar imagem
+  try {
+    gtag("event", "download");
+
+    const arqName = "figure" + new Date().getTime();
+
+    const a = document.createElement("a");
+    a.setAttribute("href", canvas.toDataURL("image/png"));
+    a.setAttribute("download", arqName);
+    a.click();
+  } catch (error) {
+    gtag("event", "exception", {
+      description: "[fn:saveImage] " + (error.message || error),
+      fatal: false,
+    });
+    alert("Não foi possível baixar a imagem: " + (error.message || error));
+  }
 }
